143d7b8ec00509b351e3ccb32f5256be88bcae44
Zachary Radtka <zachary.radtka@gmail.com>
2014-04-28 21:16:18 -0400

    ZER - Initial Commit
    Hello World

---
+++ javaRestClient/src/main/java/com/hiddentruffle/javaRestClient/App.java
@@ -0,0 +1,13 @@
+package com.hiddentruffle.javaRestClient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
