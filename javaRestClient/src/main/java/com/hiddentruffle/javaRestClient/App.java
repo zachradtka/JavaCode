44fc6beea2d7ba5b0782dc49c83d0ca47f462f4b
Zachary Radtka <zachary.radtka@gmail.com>
2014-04-29 19:40:40 -0400

    ZER - A simple GET client that contacts google

--- javaRestClient/src/main/java/com/hiddentruffle/javaRestClient/App.java
+++ javaRestClient/src/main/java/com/hiddentruffle/javaRestClient/App.java
@@ -1,20 +1,40 @@
 package com.hiddentruffle.javaRestClient;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-		HttpClient cleint = new DefaultHttpClient();
+		CloseableHttpClient client = HttpClients.createDefault();
+		
+		HttpGet httpGet = new HttpGet("http://www.google.com/search?hl=en&q=httpclient&btnG=Google+Search&aq=f&oq=");
+		CloseableHttpResponse response = null;
+		try {
+			response = client.execute(httpGet);
+			System.out.println("Response: " + response);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				response.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
-        System.out.println( "Hello World!" );
     }
 }
